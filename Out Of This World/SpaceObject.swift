506493267cbcd24f71faba77dd118b78dfe96edc
Reinaldo Haynes <reinaldoh@complex.com>
2014-08-15 11:59:22 -0400

    More xcode 6 beta 5 fixes.

--- Out Of This World/SpaceObject.swift
+++ Out Of This World/SpaceObject.swift
@@ -22,20 +22,20 @@ class SpaceObject: NSObject {
     
     var spaceImage:         UIImage?
     
-    init() {
+    override init() {
         
     }
     
     init(initWithData data:NSDictionary, andImage image:UIImage) {
-        if data[PLANET_NAME] { self.name = String(data[PLANET_NAME] as NSString) }
+        if data[PLANET_NAME] { self.name = String(format: data[PLANET_NAME] as NSString) }
         if data[PLANET_GRAVITY] { self.gravitationalForce = Float(data[PLANET_GRAVITY] as NSNumber) }
         if data[PLANET_DIAMETER] { self.diameter = Float(data[PLANET_DIAMETER] as NSNumber) }
         if data[PLANET_YEAR_LENGTH] { self.yearLength = Float(data[PLANET_YEAR_LENGTH] as NSNumber) }
         if data[PLANET_DAY_LENGTH] { self.dayLength = Float(data[PLANET_DAY_LENGTH] as NSNumber) }
         if data[PLANET_TEMPERATURE] { self.temperature = Float(data[PLANET_TEMPERATURE] as NSNumber) }
         if data[PLANET_NUMBER_OF_MOONS] { self.numberOfMoons = Int(data[PLANET_NUMBER_OF_MOONS] as NSNumber) }
-        if data[PLANET_NICKNAME] { self.nickname = String(data[PLANET_NICKNAME] as NSString) }
-        if data[PLANET_INTERESTING_FACT] { self.interestingFact = String(data[PLANET_INTERESTING_FACT] as NSString) }
+        if data[PLANET_NICKNAME] { self.nickname = String(format: data[PLANET_NICKNAME] as NSString) }
+        if data[PLANET_INTERESTING_FACT] { self.interestingFact = String(format: data[PLANET_INTERESTING_FACT] as NSString) }
         
         self.spaceImage = image
     }
